3b88e36b9ac538e52f4d78def2ef70ce4ba4e816
Foulail Oumar <100520938+FoulailOumar93@users.noreply.github.com>
2024-04-28 13:37:09 +0200

    Add files via upload

---
+++ gsbLaravel/framework/views/f67d3db81f40e67aec8ab6956fe7efcc7942f225.php
@@ -0,0 +1,53 @@
+
+    <?php $__env->startSection('menu'); ?>
+            <!-- Division pour le sommaire -->
+        <div id="menuGauche">
+            <div id="infosUtil">
+                  
+             </div>  
+              <ul id="menuList">
+                <li >
+                  <strong>Bonjour <?php echo e($comptable['nom'] . ' ' . $comptable['prenom']); ?></strong> 
+                  <li class="smenu">
+                     <a href="<?php echo e(route('chemin_validerFrais')); ?>" title="valider fiche de frais ">Valider fiche frais</a>
+                  </li>
+                <li class="smenu">
+                  <a href="<?php echo e(route('chemin_deconnexion')); ?>" title="Se déconnecter">Déconnexion</a>
+                </li>
+                <li class="smenu">
+                    <a href="<?php echo e(route('chemin_voirFraisParMoisEtParVisiteurPourUnTypeDeFrais')); ?>" title="Frais par mois et par visiteur pour type de frais">Frais par mois et par visiteur pour type de frais</a>
+                  </li>
+                <li class="smenu">
+                   <a href="<?php echo e(route('chemin_voirFraisParMoisPourUnVisiteur')); ?>" title="Frais par mois pour un visiteur">Frais par mois pour un visiteur</a>
+               </li>
+               <li class="smenu">
+                    <a href="<?php echo e(route('chemin_voirFraisParVisiteurPourUneAnne')); ?>" title="Frais par visiteur pour une année">Frais par visiteur pour une année</a>
+               </li>
+  <!--aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa--->
+                <li class="smenu">
+                     <a href="<?php echo e(route('chemin_ajouterSaisie')); ?>" title="Ajouter un visiteur">Ajouter un visiteur</a>
+                  </li>
+                  <li class="smenu">
+                    <a href="<?php echo e(route('chemin_modifierChoix')); ?>" title="Modifier un visiteur">Modifier un visiteur</a>
+                  </li>
+                  <li class="smenu">
+                     <a href="<?php echo e(route('chemin_lister')); ?>" title="Afficher la liste des visiteurs">Liste des visiteurs</a>
+                  </li>
+   <!--dddddddddddddddddddddddddddddddddddddddd--->
+                  <li class="smenu">
+                     <a href="<?php echo e(route('chemin_fraisARembourser')); ?>" title=" remboursement">Fiches à rembourser</a>
+                  </li>
+                  <li class="smenu">
+                    <a href="<?php echo e(route('chemin_listerfraishorsforfait')); ?>" title="fraishorsforfait">fraishorsforfait</a>
+                  </li>
+                  <li class="smenu">
+                    <a href="<?php echo e(route('chemin_voirVisiteurParDepartement')); ?>" title="listerVisiteurParDepartement_93">listerVisiteurParDepartement</a>
+                  </li>
+                  <li class="smenu">
+                    <a href="<?php echo e(route('chemin_supprimerChoix')); ?>" title="supprimer un visiteur">supprimer un visiteur</a>
+                  </li>
+              </ul>
+               
+        </div>
+    <?php $__env->stopSection(); ?>  
+<?php echo $__env->make('modeles/visiteur', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\laragon\www\Missions\AP\E4\gsbLaravel\gsbLaravel\resources\views/sommaireCompta.blade.php ENDPATH**/ ?>
---
+++ gsbLaravel/framework/views/f913068336b783496a87e6aa50e51dff1f77c9c0.php
@@ -0,0 +1,28 @@
+<?php $__env->startSection('contenu1'); ?>
+
+<div id="contenu">
+        <h2>Frais par mois pour un visiteur</h2>
+        <h3>Visiteur à sélectionner : </h3>
+       <form action="<?php echo e(route('chemin_afficherFraisParMoisPourUnVisiteur')); ?>" method="post">
+        <?php echo e(csrf_field()); ?> <!-- laravel va ajouter un champ caché avec un token -->
+        <div class="corpsForm"><p>
+          <label for="lstMois" >Type : </label>
+          <select id="lstMois" name="visiteur">
+              <?php $__currentLoopData = $lesVisiteurs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $unVisiteur): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <option  value="<?php echo e($unVisiteur['idVisiteur']); ?>">
+                      <?php echo e($unVisiteur['idVisiteur']); ?> 
+                    </option>
+              <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+          </select>
+        </p>
+        </div>
+        <div class="piedForm">
+        <p>
+          <input id="ok" type="submit" value="Valider" size="20" />
+        </p> 
+        </div>
+          
+        </form> 
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('sommaireCompta', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\laragon\www\Missions\AP\E4\gsbLaravel\gsbLaravel\resources\views/voirFraisParMoisPourUnVisiteur.blade.php ENDPATH**/ ?>
